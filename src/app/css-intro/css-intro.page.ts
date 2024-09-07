d81c62dae87adbb1bbee64d653e0bdc003902797
Anonymous03-sys <luigisdm.ldm@gmail.com>
2024-09-07 14:02:07 +0800

    luigi's third commit

--- src/app/css-intro/css-intro.page.ts
+++ src/app/css-intro/css-intro.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -6,7 +6,8 @@ import { MenuController } from '@ionic/angular';
   templateUrl: './css-intro.page.html',
   styleUrls: ['./css-intro.page.scss'],
 })
-export class CssIntroPage implements OnInit {
+export class CssIntroPage implements OnInit, AfterViewInit {
+
 
   isPopupVisible = false;
   showSubtopics = false;
@@ -32,4 +33,51 @@ export class CssIntroPage implements OnInit {
   ngOnInit() {
   }
 
+  changeContent() {
+    const demoElement = document.getElementById('demo');
+    if (demoElement) {
+      demoElement.innerHTML = 'Hello JavaScript!';
+    } else {
+      console.error('Element with id "demo" not found.');
+    }
+  }
+
+
+
+  ngAfterViewInit() {
+    // Ensure the element is available after the view has been initialized
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    const turnOnButton = document.querySelector('button[onclick*="LOGO.png"]');
+    const turnOffButton = document.querySelector('button[onclick*="GOOGLE-LOGO.png"]');
+
+    if (turnOnButton) {
+      turnOnButton.addEventListener('click', () => {
+        this.changeImageSrc('myImage', 'assets/img/LOGO.png');
+      });
+    }
+
+    if (turnOffButton) {
+      turnOffButton.addEventListener('click', () => {
+        this.changeImageSrc('myImage', 'assets/img/GOOGLE-LOGO.png');
+      });
+    }
+  }
+
+  changeImageSrc(elementId: string, src: string) {
+    const imgElement = document.getElementById(elementId) as HTMLImageElement;
+    if (imgElement) {
+      imgElement.src = src;
+    } else {
+      console.error(`Element with id "${elementId}" not found.`);
+    }
+  }
+
+
 }
+
+
+
+
