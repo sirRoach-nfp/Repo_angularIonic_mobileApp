13949b4d1ccdab8fb1f3e4301206f919e9b8dc2d
sirRoach-nfp <nathanieljosepascual.psjnhs@gmail.com>
2024-09-12 08:30:19 +0800

    initial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,6 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  openaiApiKey:'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'
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
