82b022fe6aa10be074ad0dfbc4f52d9f63f200ba
sirRoach-nfp <nathanieljosepascual.psjnhs@gmail.com>
2024-09-23 18:29:02 +0800

    added processenvport

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/test",(req,res)=>{
     console.log("testo")
 })
 
-const PORT = 5000;
+const PORT =  process.env.PORT ||  5000;
 
 app.listen(PORT,()=>{
     console.log("Listening to port 5000")
