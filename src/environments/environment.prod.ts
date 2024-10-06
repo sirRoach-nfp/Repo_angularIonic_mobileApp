2c05d03945edc56f3413b8cdc6df01777619dbc8
sirRoach-nfp <nathanieljosepascual.psjnhs@gmail.com>
2024-10-06 14:46:09 +0800

    second

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey:  "AIzaSyD_SMmSXXqX2Khg0Kq1clevKUuCcdgGSCo",
+    authDomain: "codetology-3836e.firebaseapp.com",
+    databaseURL: "https://codetology-3836e-default-rtdb.asia-southeast1.firebasedatabase.app/", // Optional if you're using Firestore
+    projectId: "codetology-3836e",
+    storageBucket:  "codetology-3836e.appspot.com",
+    messagingSenderId: "249575970915",
+    appId:"1:249575970915:web:458a3c380a7eac8a938056"
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
   firebase: {
     apiKey:  "AIzaSyD_SMmSXXqX2Khg0Kq1clevKUuCcdgGSCo",
